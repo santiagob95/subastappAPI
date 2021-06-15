948a3a8e7c311701f7cbe9a3c827f5cbb26221dc
santiago <santiagob95@hotmail.com>
2021-06-14 23:44:54 -0300

    implementada conexion a bd produccion o testing

--- config/db.config.js
+++ config/db.config.js
@@ -1,5 +1,6 @@
 
-module.exports ={
+
+const prod ={
     HOST: "ec2-23-20-124-77.compute-1.amazonaws.com", //antes: localhost
     USER:"jpkiutoahforpr", //antes "root"
     PASSWORD: "483ab95b7fe55d572db1649c128868302ff6b5b3c8175e9ce7b9c891e33172cd",    //"root"
@@ -16,4 +17,26 @@ module.exports ={
       acquire: 30000,
       idle: 10000
     }
-  };
+}
+
+const test = {
+    HOST: "ec2-54-158-232-223.compute-1.amazonaws.com", //antes: localhost
+    USER:"blddtzpvlqwprp", //antes "root"
+    PASSWORD: "a89aef3138dcbe3cd599f0b5c2483c5adc98b25451c12ab889e443932417d7b8",    //"root"
+    logging: true, //false
+    DB: "dfdj41tecgciji", //antes: subastapp
+    dialect: "postgres", //antes: mysql
+    dialectOptions: {
+      ssl: true
+      },
+    PORT: 5432,
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+      }
+    }
+
+
+module.exports= test
--- models/associations.js
+++ models/associations.js
@@ -1,13 +1,20 @@
-const { itemsCatalogo } = require("./index");
+const { itemsCatalogo, productos } = require("./index");
 const db = require("./index");
 const Producto = db.productos
 const Catalogo = db.catalogos
-const ItemsCatalogo = db.itemsCatalogo
+const Subasta = db.subastas
 
 
 //Item catalogo tiene un producto y un catalogo en el que esta
 // ItemsCatalogo.belongsTo(Producto);
 // ItemsCatalogo.belongsTo(Catalogo);
 
+
+//N a N 
 Producto.belongsToMany(Catalogo,{through:"ItemsCatalogo"})
 Catalogo.belongsToMany(Producto,{through:"ItemsCatalogo"})
+
+
+//1 a N
+Subasta.hasMany(Catalogo, {foreignKey:"subastaID"})
+Catalogo.belongsTo(Subasta,{foreignKey:"catalogoID"})
